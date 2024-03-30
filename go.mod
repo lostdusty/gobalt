8f022c59f990b2e201a15c861013832e3b111999
PrincessMortix <hi@princessmortix.link>
2024-03-30 03:04:44 -0300

    v1.0.1: support for custom instances, vimeoDash option

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/princessmortix/gobalt
+module github.com/lostdusty/gobalt
 
 go 1.21.5
--- gobalt.go
+++ gobalt.go
@@ -1,3 +1,4 @@
+// Package Gobalt provides a go way to communicate with https://cobalt.tools servers.
 package gobalt
 
 import (
@@ -8,6 +9,7 @@ import (
 	"net/http"
 	"net/url"
 	"regexp"
+	"runtime"
 	"strings"
 	"time"
 )
@@ -15,10 +17,10 @@ import (
 var (
 	CobaltApi    = "https://co.wuk.sh" //Override this value to use your own cobalt instance. See https://instances.hyper.lol/ for alternatives from the main instance.
 	UserLanguage = "en"                //Replace this following the ISO 639-1 standard. This downloads dubbed YouTube audio according to the language set here. Only takes effect if DubbedYoutubeAudio is set to true.
-	useragent    = "Gobalt/1.0"
+	useragent    = fmt.Sprintf("Mozilla/5.0 (%v; %v); gobalt/v1.0.2 (%v; %v); +(https://github.com/lostdusty/gobalt)", runtime.GOOS, runtime.GOARCH, runtime.Compiler, runtime.Version())
 )
 
-type serverInfo struct {
+type ServerInfo struct {
 	Version   string `json:"version"`   //cobalt version
 	Commit    string `json:"commit"`    //git commit
 	Branch    string `json:"branch"`    //git branch
@@ -40,19 +42,31 @@ type cobaltResponse struct {
 	URLs []string //If the status is picker all the urls will go here.
 }
 
+type CobaltInstances []struct {
+	Cors      int    `json:"cors"`
+	Commit    string `json:"commit,omitempty"`
+	Name      string `json:"name,omitempty"`
+	StartTime int64  `json:"startTime"`
+	API       string `json:"api"`
+	Version   string `json:"version"`
+	Branch    string `json:"branch,omitempty"`
+	FrontEnd  string `json:"frontEnd"`
+	Status    bool   `json:"status"`
+}
+
 type Settings struct {
-	Url                   string     `json:"url"`             //Any URL from bilibili.com, instagram, pinterest, reddit, rutube, soundcloud, streamable, tiktok, tumblr, twitch clips, twitter/x, vimeo, vine archive, vk or youtube. Will be url encoded later.
-	VideoCodec            codecs     `json:"vCodec"`          //H264, AV1 or VP9, defaults to H264.
-	VideoQuality          int        `json:"vQuality,string"` //144p to 2160p (4K), if not specified will default to 1080p.
-	AudioCodec            audioCodec `json:"aFormat"`         //MP3, Opus, Ogg or Wav. If not specified will default to best.
-	FilenamePattern       pattern    `json:"filenamePattern"` //Classic, Basic, Pretty or Nerdy. Defaults to Pretty
-	AudioOnly             bool       `json:"isAudioOnly"`     //Removes the video, downloads audio only. Default: false
-	//RemoveTikTokWatermark bool       `json:"isNoTTWatermark"` //Removes TikTok watermark from TikTok videos. Default: false
-	FullTikTokAudio       bool       `json:"isTTFullAudio"`   //Enables download of original sound used in a tiktok video. Default: false
-	VideoOnly             bool       `json:"isAudioMuted"`    //Downloads only the video, audio is muted/removed. Default: false
-	DubbedYoutubeAudio    bool       `json:"dubLang"`         //Pass the User-Language HTTP header to use the dubbed audio of the respective language, must change according to user's preference, default is English (US). Uses ISO 639-1 standard.
-	DisableVideoMetadata  bool       `json:"disableMetadata"` //Removes file metadata. Default: false
-	ConvertTwitterGifs    bool       `json:"twitterGif"`      //Changes whether twitter gifs are converted to .gif (Twitter gifs are usually stored in .mp4 format). Default: true
+	Url                  string     `json:"url"`             //Any URL from bilibili.com, instagram, pinterest, reddit, rutube, soundcloud, streamable, tiktok, tumblr, twitch clips, twitter/x, vimeo, vine archive, vk or youtube. Will be url encoded later.
+	VideoCodec           codecs     `json:"vCodec"`          //H264, AV1 or VP9, defaults to H264.
+	VideoQuality         int        `json:"vQuality,string"` //144p to 2160p (4K), if not specified will default to 1080p.
+	AudioCodec           audioCodec `json:"aFormat"`         //MP3, Opus, Ogg or Wav. If not specified will default to best.
+	FilenamePattern      pattern    `json:"filenamePattern"` //Classic, Basic, Pretty or Nerdy. Defaults to Pretty
+	AudioOnly            bool       `json:"isAudioOnly"`     //Removes the video, downloads audio only. Default: false
+	UseVimeoDash         bool       `json:"vimeoDash"`       //Changes whether streamed file type is preferred for vimeo videos.. Default: false
+	FullTikTokAudio      bool       `json:"isTTFullAudio"`   //Enables download of original sound used in a tiktok video. Default: false
+	VideoOnly            bool       `json:"isAudioMuted"`    //Downloads only the video, audio is muted/removed. Default: false
+	DubbedYoutubeAudio   bool       `json:"dubLang"`         //Pass the User-Language HTTP header to use the dubbed audio of the respective language, must change according to user's preference, default is English (US). Uses ISO 639-1 standard.
+	DisableVideoMetadata bool       `json:"disableMetadata"` //Removes file metadata. Default: false
+	ConvertTwitterGifs   bool       `json:"twitterGif"`      //Changes whether twitter gifs are converted to .gif (Twitter gifs are usually stored in .mp4 format). Default: true
 }
 
 type codecs string
@@ -82,38 +96,35 @@ const (
 	Pretty  pattern = "pretty"  //Looks like: Video Title (1080p, h264, youtube).mp4 | audio: Audio Title - Audio Author (soundcloud).mp3
 )
 
-/*
-	Function CreateDefaultSettings() creates the Settings struct with default values:
+//Function CreateDefaultSettings() creates the Settings struct with default values:
 
-Url: ""
-VideoCodec:            H264,
-VideoQuality:          1080,
-AudioCodec:            Best,
-FilenamePattern:       Pretty,
-AudioOnly:             false,
-FullTikTokAudio:       false,
-VideoOnly:             false,
-DubbedYoutubeAudio:    false,
-DisableVideoMetadata:  false,
-ConvertTwitterGifs:    false,
-
-You MUST set an url before calling Run().
-*/
+// Url: ""
+// VideoCodec:            H264,
+// VideoQuality:          1080,
+// AudioCodec:            Best,
+// FilenamePattern:       Pretty,
+// AudioOnly:             false,
+// FullTikTokAudio:       false,
+// VideoOnly:             false,
+// DubbedYoutubeAudio:    false,
+// DisableVideoMetadata:  false,
+// ConvertTwitterGifs:    false,
+// You MUST set an url before calling Run().
 func CreateDefaultSettings() Settings {
 
 	options := Settings{
-		Url:                   "",
-		VideoCodec:            H264,
-		VideoQuality:          1080,
-		AudioCodec:            Best,
-		FilenamePattern:       Pretty,
-		AudioOnly:             false,
-		//RemoveTikTokWatermark: false,
-		FullTikTokAudio:       false,
-		VideoOnly:             false,
-		DubbedYoutubeAudio:    false,
-		DisableVideoMetadata:  false,
-		ConvertTwitterGifs:    true,
+		Url:                  "",
+		VideoCodec:           H264,
+		VideoQuality:         1080,
+		AudioCodec:           Best,
+		FilenamePattern:      Pretty,
+		AudioOnly:            false,
+		UseVimeoDash:         false,
+		FullTikTokAudio:      false,
+		VideoOnly:            false,
+		DubbedYoutubeAudio:   false,
+		DisableVideoMetadata: false,
+		ConvertTwitterGifs:   true,
 	}
 	return options
 }
@@ -135,18 +146,18 @@ func Run(opts Settings) (*cobaltResponse, error) {
 	}
 
 	optionsPayload := Settings{
-		Url:                   url.QueryEscape(opts.Url),
-		VideoCodec:            opts.VideoCodec,
-		VideoQuality:          opts.VideoQuality,
-		AudioCodec:            opts.AudioCodec,
-		FilenamePattern:       opts.FilenamePattern,
-		AudioOnly:             opts.AudioOnly,
-		//RemoveTikTokWatermark: opts.RemoveTikTokWatermark,
-		FullTikTokAudio:       opts.FullTikTokAudio,
-		VideoOnly:             opts.VideoOnly,
-		DubbedYoutubeAudio:    opts.DubbedYoutubeAudio,
-		DisableVideoMetadata:  opts.DisableVideoMetadata,
-		ConvertTwitterGifs:    opts.ConvertTwitterGifs,
+		Url:                  url.QueryEscape(opts.Url),
+		VideoCodec:           opts.VideoCodec,
+		VideoQuality:         opts.VideoQuality,
+		AudioCodec:           opts.AudioCodec,
+		FilenamePattern:      opts.FilenamePattern,
+		AudioOnly:            opts.AudioOnly,
+		UseVimeoDash:         opts.UseVimeoDash,
+		FullTikTokAudio:      opts.FullTikTokAudio,
+		VideoOnly:            opts.VideoOnly,
+		DubbedYoutubeAudio:   opts.DubbedYoutubeAudio,
+		DisableVideoMetadata: opts.DisableVideoMetadata,
+		ConvertTwitterGifs:   opts.ConvertTwitterGifs,
 	}
 	payload, _ := json.Marshal(optionsPayload)
 
@@ -197,8 +208,8 @@ func Run(opts Settings) (*cobaltResponse, error) {
 }
 
 // This function is called before Run() to check if the cobalt server used is reachable.
-// If you can't contact the main server, you should using one of the instances listed in https://instances.hyper.lol/.
-func CobaltServerInfo(api string) (*serverInfo, error) {
+// If you can't contact the main server, try getting another instance using GetCobaltinstances().
+func CobaltServerInfo(api string) (*ServerInfo, error) {
 	//Check if the server is reachable
 	client := &http.Client{
 		Timeout: 15 * time.Second,
@@ -220,13 +231,13 @@ func CobaltServerInfo(api string) (*serverInfo, error) {
 		return nil, err
 	}
 
-	var serverResponse serverInfo
+	var serverResponse ServerInfo
 	err = json.Unmarshal(jsonbody, &serverResponse)
 	if err != nil {
 		return nil, err
 	}
 	res.Body.Close()
-	return &serverInfo{
+	return &ServerInfo{
 		Branch:    serverResponse.Branch,
 		Commit:    serverResponse.Commit,
 		Name:      serverResponse.Name,
@@ -235,4 +246,51 @@ func CobaltServerInfo(api string) (*serverInfo, error) {
 		Cors:      serverResponse.Cors,
 		StartTime: serverResponse.StartTime,
 	}, nil
+}
+
+// GetCobaltInstances makes a request to instances.hyper.lol and returns a list of all online cobalt instances.
+func GetCobaltInstances() ([]ServerInfo, error) {
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+	req, err := http.NewRequest("GET", "https://instances.hyper.lol/instances.json", nil)
+	req.Header.Add("User-Agent", useragent)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	jsonbody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var cobaltHyperInstances CobaltInstances
+	err = json.Unmarshal(jsonbody, &cobaltHyperInstances)
+	if err != nil {
+		return nil, err
+	}
+	res.Body.Close()
+
+	instancesList := make([]ServerInfo, 0)
+
+	for _, v := range cobaltHyperInstances {
+		if v.Status {
+			instancesList = append(instancesList, ServerInfo{
+				Version:   v.Version,
+				Commit:    v.Commit,
+				Branch:    v.Branch,
+				Name:      v.Name,
+				URL:       v.API,
+				Cors:      v.Cors,
+				StartTime: fmt.Sprint(v.StartTime),
+			})
+		}
+	}
+	return instancesList, nil
 }
